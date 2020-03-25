90630b8714589552ae343b2387e8e433919b0c73
Nir Yeffet <nir-github.com-h6lj@mailnicks.org>
2020-03-25 11:06:51 -0700

    adding simple http server and cli tool

---
+++ CLITool.pm
@@ -0,0 +1,143 @@
+use strict;
+
+package CLITool;
+use lib '.';
+use InLoop;
+
+use Exporter 'import';
+our @EXPORT = qw(subCli);
+
+my $ignore = [sub { 1; }];
+sub new {
+  my $cli = addHelp({ # bless
+    '(exit|quit)$' => [sub {
+      my $h = shift;
+      (fileno $h->{fh}) == 0 ? exitInLoop : delInLoop($h);
+      2; # don't show prompt
+    }, 'exit/quit - leave the program (terminate on console)'],
+    'restart$' => [sub {
+      exitInLoop;
+      exec($^X, $0, @ARGV);
+    }, 'restart - re-exec self'],
+    'echo( |$)' => [sub { CORE::say; }], # undocumented echo command, not sure what for
+    '$' => $ignore,
+    '\.\.$' => $ignore,
+    '\.\.\.$' => $ignore,
+  });
+  $cli->add($_[1]);
+}
+
+sub processCli {
+  my $cli = shift;
+  my %cli = %{$cli};
+  my $cc = $_;
+  my $success;
+  foreach my $cmd (keys %cli) {
+    $_ = $cc;
+    last if s/^$cmd// && ($success = $cli{$cmd}->[0]->(@_));
+  }
+  $_ = $cc;
+  $success;
+}
+
+sub addHelp {
+  my $cli = bless shift;
+  my $cmd = $_[0] ? "$_[0] " : '';
+  my $helpKey = '(help|\?)( |$)';
+  $cli->{$helpKey} = [sub {
+    if (/^$/ or /^\?/ ) {
+      print $_[1]->{prompt}->()."usage:\n";
+      print "$_->[1]\n" foreach sort { $a->[1] cmp $b->[1] } grep { $_->[1] ne '' } values %$cli;
+      print ".. - go back one level\n... - top level\n" if !$_[1]->{isAccum}->();
+    } else {
+      my $cmd = $_;
+      $_ .= ' ?';
+      print "No help for '$cmd'\n" if !$cli->processCli(@_);
+    }
+    1;
+  }, 'help, ? - show help'] if !$cli->{$helpKey};
+  $cli->{'$'} = [sub { $_[1]->{set}->(); } ] if !$cli->{'$'};
+  $cli->{'\.\.$'} = [sub { $_[1]->{leave}->(); }] if !$cli->{'\.\.$'};
+  $cli->{'\.\.\.$'} = [sub { $_[1]->{top}->(); }] if !$cli->{'\.\.\.$'};
+  $cli;
+}
+
+sub subCli { # Exported
+  my ($cli, $cmd) = @_;
+  addHelp($cli, $cmd); # bless
+  return sub {
+    $_[1]->{accumulate}->($cli, $cmd);
+    $cli->processCli(@_);
+  };
+}
+
+sub processLine {
+  my @clis = ( shift );
+  my @prompts = ( '' );
+  my (@cliAccum, @promptAccum);
+  my $methods = {
+    'accumulate' => sub {
+       unshift @cliAccum, $_[0];
+       unshift @promptAccum, ($promptAccum[0] || $prompts[0]).$_[1].' ';
+    }, 'set' => sub {
+       unshift @clis, @cliAccum;
+       unshift @prompts, @promptAccum;
+       1;
+    }, 'leave' => sub {
+       return 0 if @promptAccum || @prompts == 1;
+       shift @clis;
+       shift @prompts;
+       1;
+    }, 'top' => sub {
+       return 0 if @promptAccum || @prompts == 1;
+       shift @clis while (@clis > 1);
+       shift @prompts while (@prompts > 1);
+       1;
+    }, 'prompt' => sub {
+       $promptAccum[0] || $prompts[0];
+    }, 'isAccum' => sub {
+       @promptAccum || @prompts == 1;
+    },
+  };
+  return evLine {
+    @cliAccum = ();
+    @promptAccum = ();
+    my $h = shift;
+    my $fd = select($h->{out} || $h->{fh});
+    chomp; s/\s*$//; s/^\s*//; s/\s+/ /g;
+    my $success = $clis[0]->processCli($h, $methods);
+    print "Unknow command '$_'.\n" if !$success;
+    print $prompts[0].'> ' if $success != 2;
+    select($fd);
+  };
+}
+
+sub del {
+  my ($cli, $del) = @_;
+  delete $cli->{$_} foreach keys %$del;
+  $cli;
+}
+
+sub add {
+  my ($cli, $new) = @_;
+  @{$cli}{keys %$new} = values %$new if $new;
+  $cli;
+}
+
+sub console {
+  my $cli = shift;
+  $| = 1;
+  evOn {
+    my $h = shift;
+    $_ = *STDIN;
+    $h->{out} = *STDOUT;
+    print '> ';
+    1;
+  } evHup { # hup (ctrl-d)
+    print "\n";
+    exitInLoop();
+    0;
+  } $cli->processLine;
+}
+
+1;
---
+++ HTTPServer.pm
@@ -0,0 +1,120 @@
+use strict;
+
+package HTTPServer;
+use POSIX qw(strftime);
+use lib '.';
+use TCPInLoop;
+use InLoop;
+use JSON::PP;
+
+use Exporter 'import';
+our @EXPORT = qw(httpResponse httpServer);
+
+my %status = (
+  200 => '200 Ok',
+  204 => '204 No Content',
+  404 => '404 Not Found',
+  501 => '501 Not Implemented',
+);
+
+sub httpResponse {
+  my ($status, $data, $ctype, $header) = @_;
+  $status = $status{$status} || $status;
+  $data = $status."\n" if !defined($data) && $status != 204;
+  if (!defined($ctype)) {
+    if ((ref $data) =~ /^(HASH|ARRAY)$/) {
+      $ctype = 'application/json';
+      $data = eval { encode_json($data) } || $data;
+    } else {
+      $ctype = 'text/plain';
+    }
+  }
+  if ($header ne '' and !$header =~ /\r\n$/s) {
+    $header .= "\r\n";
+  }
+  return "HTTP/1.1 $status\r\n".
+         "Server: HTTPInLoop\r\n".
+         "Date: ${\strftime('%a, %d %b %Y %T GMT', gmtime())}\r\n".
+         "Content-Type: $ctype\r\n".
+         "Content-Length: ${\(length($data) * 1)}\r\n".
+         "Connection: keep-alive\r\n$header\r\n$data";
+}
+
+sub _parseCookie {
+  my $cookie = {};
+  foreach (split('; ', shift)) {
+    my ($name, $value) = split /=/;
+    $cookie->{$name} = $value;
+  }
+  return $cookie;
+}
+
+sub _parseQuerystring {
+  my $qs = {};
+  foreach (split('&', shift)) {
+    my ($name, $value) = split /=/;
+    $value =~ s/%([a-fA-F0-9]{2})/pack("C", hex($1))/eg;
+    $value =~ s/\+/ /g;
+    if ($name =~ s/\[\]$// || defined($qs->{$name})) {
+      if (ref $qs->{$name} eq 'ARRAY') {
+        push @{$qs->{$name}}, $value;
+      } elsif (defined($qs->{$name})) {
+        $qs->{$name} = [$qs->{$name}, $value];
+      } else {
+        $qs->{$name} = [$value];
+      }
+    } else {
+      $qs->{$name} = $value;
+    }
+  }
+  return $qs;
+}
+
+sub httpServer {
+  my ($port, $address, $router) = @_;
+
+  return tcpServer($port, $address, evLine {
+    my $h = $_[0];
+    $h->{data} .= $_;
+    my $rest;
+    do {
+      ($_, $rest) = split("\r\n\r\n", $h->{data}, 2);
+      return if !defined($rest); # EOH not found
+      my $cl = (split(/^content-length: /mi, $_, 2))[1] * 1;
+      my $body = substr($rest, 0, $cl, '');
+      return if length($body) < $cl; # wating for body
+      $h->{data} = $rest;
+      $h->{body} = /^content-type: application\/json\r/mi ? eval { decode_json($body); } || $body : $body;
+
+      my $ref = ref $router;
+      my $status = 501;
+      my $r = $router;
+      my $header = $_;
+      my ($next, $qs);
+      ($next ,$_) = split /[ \r]/, $_, 3;
+      ($_, $qs) = split /\?/;
+      while (1) {
+        $ref = ref $r;
+        if ($ref eq 'HASH') {
+          defined($r = $r->{$next}) or last;
+          $status = 404;
+          s/^\/([^\/]*)//;
+          $next = $1;
+        } elsif ($ref eq 'CODE' && /^\/?$/) {
+          $h->{query} = _parseQuerystring($qs) if defined($qs);
+          $_ = $header;
+          $h->{cookie} = _parseCookie($1) if /^Cookie: ([^\r]*)/mi;
+          $status = 0;
+          $r->($h);
+          delete $h->{query};
+          delete $h->{cookie};
+          last;
+        } else { last; }
+      }
+      print { $h->{fh} } httpResponse($status) if $status;
+      delete $h->{body};
+    } while ($h->{data} ne '');
+  });
+}
+
+1;
