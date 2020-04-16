4cdd181063fae90f4684cd8d56e05a366c9621c9
Nir Yeffet <niry@mailnicks.org>
2020-04-16 00:10:30 -0700

    use EPOLLOUT when appropriate

--- InLoop/methods.pm
+++ InLoop/methods.pm
@@ -9,9 +9,27 @@ sub getOpenTime { $_[0]->{openTime}; }
 sub write {
   my ($h, $d) = @_;
   my $out = $h->{out};
-  if ($out) {
-    syswrite($out, $d);
-    InLoop::_hangup($h) if $!;
+  return if !$out;
+  my $dataOut = $h->{dataOut};
+  return push(@$dataOut, $d) if $dataOut;
+  syswrite($out, $d);
+  if ($!) {
+    if ($!{EAGAIN}) {
+      $h->{dataOut} = [$d];
+      InLoop::evOutRef(sub {
+        my $h = shift;
+        my $dataOut = $h->{dataOut};
+        while (@$dataOut) {
+          syswrite($_, $dataOut->[0]);
+          return if $!;
+          shift @$dataOut;
+        }
+        delete $h->{dataOut};
+        1;
+      }, $h);
+    } else {
+      InLoop::_hangup($h);
+    }
   }
 }
 
